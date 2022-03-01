bab50b51aa8385a42fd58a2bbd46b96d0dfc3949
heat-man <manheat51@gmail.com>
2022-03-01 15:47:06 +0900

    modifying decoder.c

--- run_length_encoding/decoder.c
+++ run_length_encoding/decoder.c
@@ -6,29 +6,27 @@ int main(int argc, char* argv[]){
 		printf("usage: decoder.exe [file to decode] [output file]\n");
 		return 0;
 	}
-	FILE *in;
-	FILE *out;
-	char file_in[100], file_out[100], _count;
+	FILE *input;
+	FILE *output;
+	char _count;
 	int nowChar, prevChar, count;
-	scanf("%s", file_in);
-	scanf("%s", file_out);
-	in = fopen(file_in, "rb");
-	out = fopen(file_out, "wb");
+	input = fopen(argv[0], "rb");
+	output = fopen(argv[1], "wb");
 	prevChar = -1;
 	while(1){
-		nowChar = fgetc(in);
+		nowChar = fgetc(input);
 		if(nowChar == -1) break;
-		fputc(nowChar, out);
+		fputc(nowChar, output);
 		if(nowChar == prevChar){
-			_count = fgetc(in);
+			_count = fgetc(input);
 			if(_count == -1) break;
 			count = _count - '0';
-			for(int i=0; i<count; i++) fputc(nowChar, out);
+			for(int i=0; i<count; i++) fputc(nowChar, output);
 		}
 		prevChar = nowChar;
 	}
-	fclose(in);
-	fclose(out);
+	fclose(input);
+	fclose(output);
 	return 0;
 }		
 	
